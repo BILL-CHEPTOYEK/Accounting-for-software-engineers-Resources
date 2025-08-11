0160e8153f3503f77f8bf8e72fcb2cbc3067da8f
Bill Cheptoyek <108127528+BILL-CHEPTOYEK@users.noreply.github.com>
2025-08-11 13:14:29 +0000

    set up database models

--- 04-Application/backend/models/index.js
+++ 04-Application/backend/models/index.js
@@ -4,6 +4,9 @@ const sequelize = require('../config/database');
 const Party = require('./party');
 const Invoice = require('./invoice');
 const Bill = require('./bill');
+const AccountType = require('./accountType');
+const ChartOfAccount = require('./chartOfAccount');
+const Transaction = require('./transaction'); 
 
 const db = {};
 
@@ -11,30 +14,65 @@ db.sequelize = sequelize;
 db.Party = Party;
 db.Invoice = Invoice;
 db.Bill = Bill;
+db.AccountType = AccountType;
+db.ChartOfAccount = ChartOfAccount;
+db.Transaction = Transaction; 
 
 // Define Associations
-// A Party can have many Invoices
+
+// Party associations
 db.Party.hasMany(db.Invoice, {
   foreignKey: 'party_id',
   as: 'invoices',
 });
-
-// An Invoice belongs to a single Party
 db.Invoice.belongsTo(db.Party, {
   foreignKey: 'party_id',
   as: 'customer',
 });
 
-// A Party can have many Bills
 db.Party.hasMany(db.Bill, {
   foreignKey: 'party_id',
   as: 'bills',
 });
-
-// A Bill belongs to a single Party
 db.Bill.belongsTo(db.Party, {
   foreignKey: 'party_id',
   as: 'issuer',
+});
+
+// Chart of Accounts associations
+db.AccountType.hasMany(db.ChartOfAccount, {
+  foreignKey: 'account_type_id',
+  as: 'accounts',
+});
+db.ChartOfAccount.belongsTo(db.AccountType, {
+  foreignKey: 'account_type_id',
+  as: 'accountType',
+});
+
+// Self-referencing association for parent accounts
+db.ChartOfAccount.hasMany(db.ChartOfAccount, {
+  foreignKey: 'parent_id',
+  as: 'childAccounts',
+});
+db.ChartOfAccount.belongsTo(db.ChartOfAccount, {
+  foreignKey: 'parent_id',
+  as: 'parentAccount',
+});
+
+// Transaction associations
+db.ChartOfAccount.hasMany(db.Transaction, {
+  foreignKey: 'account_id',
+  as: 'transactions',
+});
+db.Transaction.belongsTo(db.ChartOfAccount, {
+  foreignKey: 'account_id',
+  as: 'account',
+});
+
+// Self-referencing association for transaction reversals
+db.Transaction.hasOne(db.Transaction, {
+  foreignKey: 'reversal_of_transaction_id',
+  as: 'reversedTransaction',
 });
 
 module.exports = db;
