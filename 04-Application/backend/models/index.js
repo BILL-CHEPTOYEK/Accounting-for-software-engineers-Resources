ce69677df72320e586040a28d7452f1bbedf647d
Bill Cheptoyek <108127528+BILL-CHEPTOYEK@users.noreply.github.com>
2025-08-11 12:52:25 +0000

    set up database models

--- 04-Application/backend/models/index.js
+++ 04-Application/backend/models/index.js
@@ -2,13 +2,25 @@
 
 const sequelize = require('../config/database');
 const Party = require('./party');
+const Invoice = require('./invoice');
 
 const db = {};
 
 db.sequelize = sequelize;
 db.Party = Party;
+db.Invoice = Invoice;
 
-// Associations
+// Define Associations
+// A Party can have many Invoices
+db.Party.hasMany(db.Invoice, {
+  foreignKey: 'party_id',
+  as: 'invoices',
+});
 
+// An Invoice belongs to a single Party
+db.Invoice.belongsTo(db.Party, {
+  foreignKey: 'party_id',
+  as: 'customer', // Using customer as the alias for clarity
+});
 
 module.exports = db;
