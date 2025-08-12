43bc1db60e664a6d78c97e191a7d7f5fa283546e
katocollins <ckato131@gmail.com>
2025-08-12 19:33:24 +0300

    frontend

--- 04-Application/frontend/src/pages/BranchPage.jsx
+++ 04-Application/frontend/src/pages/BranchPage.jsx
@@ -8,7 +8,7 @@ function BranchPage() {
       <h2 className="h3 fw-semibold text-dark mb-3">
         <i className="bi bi-building-fill me-3 text-secondary"></i> Branches
       </h2>
-      <p className="text-muted">Manage your various business branches or locations from this page.</p>
+      <p className="text-muted">Manage your various business branches or locations from this pag.</p>
       <div className="mt-4">
         <button className="btn btn-primary me-2">View Branches</button>
         <button className="btn btn-secondary">Add New Branch</button>
