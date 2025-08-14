9fdb912f73ee4f2f32db190d090e7b0f83a2853c
BILL-CHEPTOYEK <billcheptoyek60@gmail.com>
2025-08-14 17:10:49 +0300

    app layout

--- 04-Application/frontend/src/components/AppLayout.jsx
+++ 04-Application/frontend/src/components/AppLayout.jsx
@@ -7,7 +7,7 @@ import PartyPage from '../pages/PartyPage';
 import InvoicePage from '../pages/InvoicePage';
 import InvoiceFormPage from '../pages/InvoiceFormPage';
 import BillPage from '../pages/BillPage'; 
-import BillFormPage from '../pages/BillFormPage'; // NE
+import BillFormPage from '../pages/BillFormPage'; // N
 import TransactionPage from '../pages/TransactionPage';
 import RecordJournalEntryPage from '../pages/RecordJournalEntryPage';
 import AccountTypePage from '../pages/AccountTypePage';
