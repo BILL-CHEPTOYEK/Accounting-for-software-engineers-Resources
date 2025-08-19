fd15812a1062daecb95f0a79af3326dcd55c2081
BILL-CHEPTOYEK <billcheptoyek60@gmail.com>
2025-08-19 19:30:54 +0300

    parties page

--- 04-Application/frontend/src/pages/PartyPage.jsx
+++ 04-Application/frontend/src/pages/PartyPage.jsx
@@ -38,7 +38,7 @@ function PartyPage() {
 
   // Handler for opening the Add Party modal
   const handleAddParty = () => {
-    setCurrentParty(null); // Clear any existing party data/ha
+    setCurrentParty(null); // Clear any existing party data/han
     setShowAddEditModal(true);
   };
 
