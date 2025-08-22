ff1733139a2ace3f9d8da2af5b93d7b6d1ff37b8
BILL-CHEPTOYEK <billcheptoyek60@gmail.com>
2025-08-22 17:52:20 +0300

    user mgt and auth

--- 04-Application/backend/controllers/userController.js
+++ 04-Application/backend/controllers/userController.js
@@ -185,7 +185,7 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-// Signup user (same as createUser but with token gener
+// Signup user (same as createUser but with token gene
 exports.signupUser = async (req, res) => {
   try {
     const { email, password, first_name, last_name, role, branch_id } = req.body; 
